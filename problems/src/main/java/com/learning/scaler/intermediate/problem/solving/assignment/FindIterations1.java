b66809c1c4ee80fd417447f365314c85e8ef3179
rajpiyush220 <rajpiyush220@gmail.com>
2023-10-29 09:05:59 +0530

    Content updated

---
+++ problems/src/main/java/com/learning/scaler/intermediate/problem/solving/assignment/FindIterations1.java
@@ -0,0 +1,18 @@
+package com.learning.scaler.intermediate.problem.solving.assignment;
+
+public class FindIterations1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    * Count no of iterations for below code block and options are
+    * 1. N 2. N/2 3. sqrt(N) 4. logN
+    * */
+    public int countIterations(int N){
+        for(int i = 1; i <= N; i++){
+            if(i * i == N) return i;
+        }
+    }
+}
