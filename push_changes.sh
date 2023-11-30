6b1cddd36bd48b3e928e9d144bda5149af1b607a
rajpiyush220 <rajpiyush220@gmail.com>
2023-11-30 12:52:29 +0530

    Content upadated

--- push_changes.sh
+++ push_changes.sh
@@ -1,8 +1,13 @@
+echo "Adding ssh key"
 cd ~/.ssh/
 eval "$(ssh-agent -s)"
 ssh-add id_rajpiyush
+echo "Moving back to working dir"
 cd /g/Scaler/repo/Algorithms/
+echo "Check if any changes are there in remote repo"
 git pull
+echo "Adding all local changes to commit"
 git add .
 git commit -m "Content upadated"
+echo "Pusing local changes to git remote repo"
 git push
