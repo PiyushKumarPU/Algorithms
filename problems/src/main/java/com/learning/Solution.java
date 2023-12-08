e6ee650fc8994c346995aba58227f01f9fc996d2
rajpiyush220 <rajpiyush220@gmail.com>
2023-12-08 14:52:04 +0530

    Content upadated

--- problems/src/main/java/com/learning/Solution.java
+++ problems/src/main/java/com/learning/Solution.java
@@ -1,49 +1,34 @@
 package com.learning;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import com.learning.scaler.advance.module3.ListNode;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(solve(List.of(36, 13, 13, 26, 37, 28, 27, 43, 7)));
     }
 
-    public static ArrayList<Integer> solve(List<Integer> A) {
-        System.out.println(String.valueOf(A.stream().sorted(Comparator.comparing(Solution::factor))
-                .collect(Collectors.toCollection(ArrayList::new))));
+    static int  lPalin(ListNode A)
+    {
+        ListNode slow = A;
+        int ispalin = 1;
+        Stack<Integer> stack = new Stack<Integer>();
 
-
-    /*return A.stream().collect(Collectors.toMap(x -> x, Solution::factor))
-                .entrySet().stream().sorted(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey).collect(Collectors.toCollection(ArrayList::new));*/
-      List<Integer> factors =  A.stream().map(Solution::factor)
-              .collect(Collectors.toList());
-      System.out.println(factors);
-
-        return A.stream().sorted((o1, o2) -> {
-            if (factor(o1) < factor(o2)) {
-                return -1;
-            } else if (factor(o1) > factor(o2)) {
-                return 1;
-            } else {
-                return o1 - o2;
-            }
-        }).collect(Collectors.toCollection(ArrayList::new));
-
-    }
-
-    private static Integer factor(Integer N) {
-        int cnt = 0;
-        for (int i = 1; i * i <= N; i++) {
-            if (N % i == 0) {
-                cnt += (i == N / i) ? 1 : 2;
-            }
+        while (slow != null) {
+            stack.push(slow.val);
+            slow = slow.next;
         }
-        return cnt;
+        while (A != null) {
+            int i = stack.pop();
+            if (A.val != i) {
+                ispalin = 0;
+                break;
+            }
+            A = A.next;
+        }
+        return ispalin;
     }
 }
  
